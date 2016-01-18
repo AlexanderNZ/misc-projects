a4e12e04f044609d044bc43d375f5006dfc341f1
alexc <alexcorkin@gmail.com>
2016-01-19 12:07:53 +1300

    list item generation working. need to reimplement with ironlist

--- gulpfile.js
+++ gulpfile.js
@@ -237,7 +237,7 @@ gulp.task('clean', function() {
 });
 
 // Watch files for changes & reload
-gulp.task('serve', ['lint', 'styles', 'elements'], function() {
+gulp.task('serve', ['styles', 'elements'], function() {
   browserSync({
     port: 5000,
     notify: false,
